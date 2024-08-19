0ef7e7dc8ed47209c296369d1f250f8e24d8784f
GonzalosGitH <lacangonzalo@gmail.com>
2024-08-19 16:15:31 -0600

    Se agrega versonamiento de Java

---
+++ Ejemplo1/src/main/java/com/mycompany/ejemplo1/Ejemplo1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejemplo1;
+
+/**
+ *
+ * @author lacan
+ */
+public class Ejemplo1 {
+    
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+        System.out.println("Hello word");
+    }
+}
+
+
+        
